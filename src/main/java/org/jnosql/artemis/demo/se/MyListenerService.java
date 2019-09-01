d073b0ea68dd5a566b24ccd1f2f6922feb66e93b
Otavio Santana <otaviopolianasantana@gmail.com>
2019-09-01 20:02:19 -0300

    removes start config

--- src/main/java/org/jnosql/artemis/demo/se/MyListenerService.java
+++ src/main/java/org/jnosql/artemis/demo/se/MyListenerService.java
@@ -2,17 +2,12 @@ package org.jnosql.artemis.demo.se;
 
 import org.aerogear.kafka.cdi.annotation.Consumer;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.json.JsonObject;
 
 @ApplicationScoped
 public class MyListenerService {
 
-    @PostConstruct
-    public void init() {
-        System.out.println("start ok");
-    }
     @Consumer(topics = Config.TOPIC, groupId = Config.GROUP_ID)
     public void receiver(final JsonObject message) {
         final MyEntity entity = JsonUtils.fromJson(message, MyEntity.class);
