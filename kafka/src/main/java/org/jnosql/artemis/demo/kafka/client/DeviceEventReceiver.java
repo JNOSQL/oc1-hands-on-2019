3ca0cf26f1ca79b9e78fb03109ddc70453c9fdd1
Leonardo de Moura Rocha Lima <1250061+leomrlima@users.noreply.github.com>
2019-09-14 08:58:36 -0700

    Creates simple event receiver

---
+++ kafka/src/main/java/org/jnosql/artemis/demo/kafka/client/DeviceEventReceiver.java
@@ -0,0 +1,29 @@
+package org.jnosql.artemis.demo.kafka.client;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.UUID;
+
+import org.jnosql.artemis.demo.kafka.Bootstrap;
+import org.jnosql.artemis.demo.kafka.KafkaConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeviceEventReceiver {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DeviceEventReceiver.class);
+
+	public static void main(String[] args) {
+		String applicationName = args.length > 0 ? args[0] : UUID.randomUUID().toString();
+		String configLocation = args.length > 1 ? args[1]
+				: System.getProperty("user.home") + File.separator + ".ccloud" + File.separator + "config.oc1";
+
+		Bootstrap bootstrap = new Bootstrap(applicationName, configLocation);
+		KafkaConnection connection = new KafkaConnection("temperatureReadings", bootstrap);
+
+		while (true) {
+			connection.poll(Duration.ofDays(1), (temperature) -> logger.info("New record: {}", temperature));
+		}
+		
+	}
+}
